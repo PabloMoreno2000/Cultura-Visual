084963317fa34a155387380fe88ead1c8052b3ea
alvariux23 <alvaro.santana23@gmail.com>
2020-05-23 23:04:42 -0700

    agregue pantalla de estadisticas, FALTA FUNCIONALIDAD

---
+++ Cultura-Visual/Cultura-Visual/estadisticasViewController.swift
@@ -0,0 +1,53 @@
+//
+//  estadisticasViewController.swift
+//  Cultura-Visual
+//
+//  Created by user168641 on 5/23/20.
+//  Copyright © 2020 CVDevs. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class estadisticasViewController: UIViewController {
+
+    var subjects = ["Arquitectura", "Música", "Tema 3", "Tema 4"]
+    
+    @IBOutlet var lbSubjects: [UILabel]!
+    
+    @IBOutlet var vwBarritas: [UIView]!
+    
+    //MARK: Falta cargar los datos, y pasar todos los labels a traves de la funcion de "barraprogress" con las respuestas correctas e incorrectas
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for i in 0...3{
+            lbSubjects[i].text = subjects[i]
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func cargarDatos(){
+        
+    }
+    
+    func barraProgress(correctas: Int, incorrectas: Int) -> Double{
+        let total = Double(correctas + incorrectas)
+        let newWidth = Double(correctas) / total
+        return Double(163 * newWidth)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
